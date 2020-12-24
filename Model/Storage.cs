7fa1cc235ee712a9bcf67f539f4b65951e732359
Peter <peter.pakkert@outlook.com>
2020-12-24 15:47:52 +0100

    6.1 completed

---
+++ Model/Storage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Storage<T>
+    {
+        private List<T> _list = new List<T>();
+
+        public void AddList(T element)
+        {
+            _list.Add(element);
+        }
+    }
+}
