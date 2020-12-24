197e604b9cca789dba1804ead5b2c4ea0f8c1a1d
Peter <peter.pakkert@outlook.com>
2020-12-24 15:54:00 +0100

    6.2 complete

---
+++ Model/Score.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Score
+    {
+        public int Points { get; set; }
+        public String Name { get; set; }
+
+    }
+}
---
+++ Model/Time.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Time
+    {
+        public String Name { get; set; }
+        public TimeSpan TimeSpan { get; set; }
+    }
+}
